8358ceaa0b6ac9efc9db501bae55ab135b01316c
DENIS <denis-t02@mail.ru>
2024-05-12 16:53:33 +0300

    /

--- lr3_10.sql
+++
@@ -1,9 +0,0 @@
---Пропустить 15 строк из результирующего набора, полученного в п.9.
-
-select "BusinessEntityID"
-from "Person"."Person"
-intersect
-select "BusinessEntityID"
-from "HumanResources"."EmployeePayHistory"
-order by "BusinessEntityID" 
-offset 15
